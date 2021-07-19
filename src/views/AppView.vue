05953960e7ae24d4297b429b5f2d534c25b485a4
MattB09 <mattbancroft03@gmail.com>
2021-07-19 22:55:08 +0900

    Created AppView view. This will become the route that contains the SPA.

---
+++ src/views/AppView.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Freelance Manager</h1>
+  <h2>Clients</h2>
+  <h2>Projects</h2>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
+
