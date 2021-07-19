3311dd26f65a7c4ba55ad8775d1d5975dcaac8c1
MattB09 <mattbancroft03@gmail.com>
2021-07-19 18:20:58 +0900

    Set up template for nav and footer.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <div id="nav" class="bg-green-500">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="flex flex-col min-h-screen">
+    <nav id="nav" class="">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <main class="flex-grow">
+      <router-view/>
+    </main>
+    <footer>
+      <p class="text-center p-4 border-t border-gray-300">
+        Placeholder for footer component
+      </p>
+    </footer>
   </div>
-  <router-view/>
 </template>
 
 <style>
