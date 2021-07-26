6773ca6347523c78b7f9921d0d7d5c8bedb0d49d
MattB09 <mattbancroft03@gmail.com>
2021-07-26 18:47:17 +0900

    Created basic grid for displaying tasks and a button for recording time.

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -3,19 +3,38 @@
     <h1 class="text-2xl font-semibold mt-0 mb-2">{{ project && project.name }}</h1>
     <input type="checkbox" id="taskComplete" v-model="showCompleted">
     <label for="taskComplete">Show completed tasks</label>
-    <div v-for="task in filteredTasks" :key="task.id"
-      class="grid grid-cols-12 gap-2">
-      <h2 class="col-span-4">{{ task.name }}</h2>
+    <!-- Header for grid of tasks... -->
+    <div class="grid grid-cols-7 gap-2 border-b items-center">
+      <span class="col-span-1"></span>
+      <span class="col-span-3">Name</span>
+      <span class="col-span-1">Complete</span>
+      <span class="col-span-1">Billable</span>
+      <span class="col-span-1">Duration</span>
     </div>
+    <!-- Actual Task data -->
+    <div v-for="task in filteredTasks" :key="task.id"
+      class="grid grid-cols-7 gap-2 items-center">
+      <span class="col-span-1"><Button :text="'Record'" /></span>
+      <h2 class="col-span-3">{{ task.name }}</h2>
+      <span class="col-span-1">{{ task.isComplete ? 'Completed' : ''}}</span>
+      <span class="col-span-1">{{ task.isBillable ? 'Billable' : ''}}</span>
+      <span class="col-span-1">{{ task.duration + ' mins'}} </span>
+    </div>
+    <!-- if no tasks -->
+    <h2 v-if="!filteredTasks.length">There are no tasks that meet your selection.</h2>
   </div>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent, ref } from 'vue';
 import { useStore } from '@/store';
+import Button from '@/components/Button.vue';
 
 export default defineComponent({
   name: 'SingleProject',
+  components: {
+    Button,
+  },
   setup() {
     const store = useStore();
     const showCompleted = ref<boolean>(false);
