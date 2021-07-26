57f60510d2017a61348c527ab9409233de54e287
MattB09 <mattbancroft03@gmail.com>
2021-07-26 15:37:13 +0900

    Created dumb component for button.

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <button
+    :class="`bg-purple-400 px-3 py-1 rounded m-2 inline-block ${isHidden ? 'hidden': ''}`"
+
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Button',
+  props: ['text', 'hidden'],
+  setup(props) {
+    const isHidden = computed(() => (props.hidden === true && true));
+    console.log('isHidden', isHidden);
+    return { isHidden };
+  },
+});
+</script>
